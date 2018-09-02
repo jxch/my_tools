e0172dd5afe7f5fc148b3a50f649fd97352dd6ec
xch_Jiang <15553598935@163.com>
2018-09-03 00:46:24 +0800

    测试斐波那契数列的各个求和算法

--- src/test/test.java
+++ src/test/test.java
@@ -1,11 +1,25 @@
 package test;
 
+import tools.math.AlgorithmParam;
 import tools.math.series.Fibonacci;
 import tools.math.series.Series;
 
 public class test {
     public static void main(String[] args) {
-        Series series = new Fibonacci(100000000);
-        System.out.println(series.getSum());
+        Series series = new Fibonacci(500);
+        long startTime = System.currentTimeMillis();
+        System.out.println(series.getSum(AlgorithmParam.LOOP));
+        long endTime = System.currentTimeMillis();
+        System.out.println("LOOP : " + (endTime - startTime));
+
+//        startTime = System.currentTimeMillis();
+//        System.out.println(series.getSum(AlgorithmParam.RECURSION));
+//        endTime = System.currentTimeMillis();
+//        System.out.println("RECURSION : " + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+        System.out.println(series.getSum(AlgorithmParam.OPTIMAL));
+        endTime = System.currentTimeMillis();
+        System.out.println("OPTIMAL : " + (endTime - startTime));
     }
 }
