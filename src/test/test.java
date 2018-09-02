3caafd85ea46a2a264d1bba41bb6d56785612aff
jxch <15553598935@163.com>
2018-09-02 22:31:14 +0800

    test Fibonacci求和

--- src/test/test.java
+++ src/test/test.java
@@ -1,7 +1,11 @@
 package test;
 
+import tools.math.series.Fibonacci;
+import tools.math.series.Series;
+
 public class test {
     public static void main(String[] args) {
-
+        Series series = new Fibonacci(100000000);
+        System.out.println(series.getSum());
     }
 }
